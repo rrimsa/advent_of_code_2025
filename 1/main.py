2f7c8b24dccb415c353310936f47bd5372e640f4
Radek <radoslav.rimsa@mt.com>
2025-12-12 11:48:53 +0100

    Solved day1 part 1.

---
+++ 1/main.py
@@ -0,0 +1,21 @@
+def main():
+    moves = []
+    with open("input.txt", "r") as input:
+        moves = input.read().splitlines()
+    moves = [m.lower().strip() for m in moves]
+
+    position = 50
+    move_positions = []
+    for m in moves:
+        move_int = int(m.replace("r", "").replace("l", "-"))
+        position = (100 + position + move_int) % 100
+        move_positions.append({
+            "move": m,
+            "pos": position
+        })
+
+    print(len([m for m in move_positions if m["pos"] == 0]))
+
+
+if __name__ == "__main__":
+    main()
