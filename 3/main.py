2a9b17b8aecef8615ef1845d744213c88f34e4d5
Radek <radoslav.rimsa@mt.com>
2026-01-13 00:34:38 +0100

    Solved day 3 part 2.

--- 3/main.py
+++ 3/main.py
@@ -1,23 +1,25 @@
-def part1():
+def get_sum(limit: int):
     banks = []
     with open('input.txt', mode='r') as input:
         banks = input.read().splitlines()
         banks = [[{'i': i, 'v': int(v)} for i, v in enumerate(list(b))] for b in banks]
 
     int_list = []
-
     for bank in banks:
-        v_max_1 = max(bank, key=lambda b: b['v'])
-        if v_max_1['i'] + 1 == len(bank):
-            v_max_2 = max(bank[:v_max_1['i']], key=lambda b: b['v'])
-        else:
-            v_max_2 = max(bank[v_max_1['i'] + 1:], key=lambda b: b['v'])
-        maxes = [v_max_1, v_max_2]
-        i_sort = sorted(maxes, key=lambda b: b['i'])
-        int_list.append(int(''.join([str(i['v']) for i in i_sort])))
-
+        nums = []
+        for b in bank:
+            while (
+                nums
+                and nums[-1] < b['v']
+                and len(nums) - 1 + len(bank[b['i']:]) >= limit
+            ):
+                nums.pop()
+            if len(nums) < limit:
+                nums.append(b['v'])
+        int_list.append(int(''.join([str(i) for i in nums])))
     print(sum(int_list))
 
 
 if __name__ == "__main__":
-    part1()
+    get_sum(limit=2)
+    get_sum(limit=12)
